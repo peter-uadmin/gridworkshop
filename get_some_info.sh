4cf03832b151600a7ff3c88ad276f24369dc244f
peter <peter@uadmin.nl>
2025-12-13 12:22:05 -0500

    get_some_info.sh

--- get_some_info.sh
+++ get_some_info.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 #first get token and export it
 #then run curl and use the token
-
+clear
+#get bearer token
+echo "full list at grid manager apic documentation"
+echo "to get bearer token: api/v3/authorize" 
+echo -n "get bearer token (enter) : "
+read
 export token=`curl -s -k -X POST "https://192.168.4.230:443/api/v3/authorize" \
 -H "accept: application/json" \
 -H "Content-Type: application/json" \
@@ -9,37 +14,28 @@ export token=`curl -s -k -X POST "https://192.168.4.230:443/api/v3/authorize" \
 echo 
 
 echo "using bearer $token"
-echo "getting tenants..."
+
+echo -n "list /api/v3/accounts  (enter) "
+read
 echo ""
 curl -s -X GET "https://192.168.4.230:443/api/v3/grid/accounts" -H "accept: application/json"  -H "Authorization: Bearer $token" -k  |jq '.data[] | {name: .name, id: .id}' 
 
-echo ========
-echo "endpoints..."
-echo ""
-curl -s -X GET "https://192.168.4.230/api/v4/org/endpoints" -H "accept: application/json"  -H "X-Csrf-Token: 911ebe95-993f-466f-b46c-73fc9b95c880" -k  |jq '.data[] | {name: .name, id: .id}' 
-
-
-
-echo "Users"
+echo -n "list grid /api/v3/grid/users (enter) :"
+read
 curl -s -X GET "https://192.168.4.230:443/api/v3/grid/users" -H "accept: application/json"  -H "Authorization: Bearer $token" -k  |jq '.data[]|{name: .fullName, id: .id}' 
 
-echo ========
+echo -n "list grid /api/v3/tenant/users (enter) :"
+read
+curl -s -X GET "https://192.168.4.230:443/api/v3/tenant/users" -H "accept: application/json"  -H "Authorization: Bearer b100d270-38ff-452b-ad02-b7a72d665e31" -k  |jq '.data[]|{name: .fullName, id: .id}' 
 
-echo "nodes"
+echo -n "list /api/v3/grid/node-health (enter) :"
+read
 curl -s -X GET "https://192.168.4.230:443/api/v3/grid/node-health" -H "accept: application/json"  -H "Authorization: Bearer $token" -k  | jq '.data[] | {name: .name, id: .id}' 
 
-echo "alerts"
-curl -s -X GET "https://192.168.4.230:443/api/v3/grid/alerts" \
-  -H "accept: application/json" \
-  -H "Authorization: Bearer $token" \
-  -k | jq '.data[] | {id: .id, name: .name, severity: .severity}'
-echo ========
+echo -n "list config/product-version  (enter) :"
+read
+curl -s -X GET "https://192.168.4.230:443/api/v3/grid/config/product-version" -H "accept: application/json"  -H "Authorization: Bearer $token" -k  | jq # '.data[] | {name: .name, id: .id}' 
 
-echo "buckets"
-
-export token=`curl -s -k -X POST "https://192.168.4.230:443/api/v3/authorize" \
-  -H "accept: application/json" \
-  -H "Content-Type: application/json" \
-  -d '{"username": "root", "password": "br@@mspun111", "accountId": "06720005734697258898"}' \
-  | jq -r '.data'`
-
+echo -n "list versions (enter) :"
+read
+curl -s -X GET "https://192.168.4.230:443/api/v3/versions" -H "accept: application/json"  -H "Authorization: Bearer $token" -k  | jq # '.data[] | {name: .name, id: .id}' 
