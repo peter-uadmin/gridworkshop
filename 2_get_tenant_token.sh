3322440c0aae02a2ac99f6e54f781174cd65b60f
peter <peter@uadmin.nl>
2025-12-14 05:08:09 -0500

    2_get_tenant_token.sh

---
+++ 2_get_tenant_token.sh
@@ -0,0 +1,8 @@
+curl -k -s \
+  -X POST "https://192.168.4.230/api/v3/authorize" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "username": "root",
+    "password": "br@@mspun111",
+    "accountId": "06720005734697258898"
+
