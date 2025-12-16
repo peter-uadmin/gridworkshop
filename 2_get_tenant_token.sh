ef0b14530c8afe5d290c88d22488711a19e120d2
peter <peter@uadmin.nl>
2025-12-16 09:00:05 -0500

    2_get_tenant_token.sh

--- 2_get_tenant_token.sh
+++ 2_get_tenant_token.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/bash
 
+echo "this lists all tenant ids,"
+echo "buffer an id and paste it."
+echo -n "press enter"
+read
+./3_get_tenant_ids.sh
+echo -n "id: "
+read t_id
+export t_id
+#get tenant_ids
+#get token
+echo "$t_id"
 curl -k -s \
   -X POST "https://192.168.4.230/api/v3/authorize" \
   -H "Content-Type: application/json" \
-  -d '{
+  -d @- <<EOF
+    {
     "username": "root",
     "password": "br@@mspun111",
-    "accountId": "06720005734697258898"
-     }'
-
+    "accountId": "$t_id"
+     }
+EOF
