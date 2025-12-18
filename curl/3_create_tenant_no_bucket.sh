9b557dc3f0c860a2cb4186ab1185277131ccb809
peter <peter@uadmin.nl>
2025-12-18 12:18:28 -0500

    curl

---
+++ curl/3_create_tenant_no_bucket.sh
@@ -0,0 +1,39 @@
+ADDR=192.168.4.230
+GRID_USER="root"                 # grid admin
+GRID_PASS="br@@mspun111"
+echo -n "tenantname: "
+read tenant
+echo -n "password: "
+read password
+# 1) Get grid admin token
+GRID_TOKEN=$(curl -k -s -X POST "https://${ADDR}/api/v3/authorize" \
+  -H "Content-Type: application/json" \
+  -d "{
+        \"username\": \"${GRID_USER}\",
+        \"password\": \"${GRID_PASS}\",
+        \"cookie\": false,
+        \"csrfToken\": false
+      }" | jq -r '.data')
+
+curl -k -X POST \
+  "https://192.168.4.230/api/v4/grid/accounts" \
+  -H "Authorization: Bearer ${GRID_TOKEN}" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d @- <<EOF
+{
+  "name": "${tenant}",
+  "description": "Email address: test@example.com",
+  "capabilities": [
+    "management",
+    "s3"
+  ],
+  "policy": {
+    "useAccountIdentitySource": false,
+    "allowPlatformServices": false,
+    "allowSelectObjectContent": false,
+    "quotaObjectBytes": 100000000000
+  },
+  "password": "${password}"
+}
+EOF
