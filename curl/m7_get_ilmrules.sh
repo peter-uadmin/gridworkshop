12127ee335ecf7f0909044f3a59553197352200d
peter <peter@uadmin.nl>
2025-12-21 02:08:54 -0500

    m7_get_ilmrules.sh

---
+++ curl/m7_get_ilmrules.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#first get token and export it
+#then run curl and use the token
+clear
+#get bearer token
+echo "getting bearer token"
+export token=`curl -s -k -X POST "https://192.168.4.230:443/api/v3/authorize" \
+-H "accept: application/json" -H "Content-Type: application/json" -d '{"username": "root", "password": "br@@mspun111"}'|jq -r '.data'` echo 
+echo "use this token... $token press enter: "
+read
+
+echo "using bearer $token"
+curl -k -s -X 'GET' 'https://192.168.4.230/api/v4/grid/ilm-rules' -H 'accept: application/json'  -H "Authorization: Bearer $token" -H 'X-Csrf-Token: de162573f57192c02670757fa32583f3'
