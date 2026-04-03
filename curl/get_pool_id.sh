fecc5a8daf69afeca23f8a8d53013bb486808936
peter <peter@uadmin.nl>
2026-04-03 17:56:30 -0400

    add

--- curl/get_pool_id.sh
+++ curl/get_pool_id.sh
@@ -1,1 +1,18 @@
-curl -k -s   -H "Authorization: Bearer 32e2e64f-8b07-4e00-9852-692fd972b72e"   https://192.168.0.80/api/v4/grid/ilm-rules
+#!/bin/bash
+
+echo "Get bearer token:"
+token=$(curl -s -k -X POST "https://192.168.4.230/api/v3/authorize" \
+  -H "accept: application/json" \
+  -H "Content-Type: application/json" \
+  -d '{"username":"root","password":"Netapp1!"}' | jq -r '.data')
+
+curl -s -k \
+  -H "Authorization: Bearer $token" \
+  "https://192.168.4.230/api/v4/grid/ilm-policies" |
+jq '.data[] | {name, id, active}'
+
+#or for an even tighter output...
+#curl -s -k \
+#  -H "Authorization: Bearer $token" \
+#  "https://192.168.4.230/api/v4/grid/ilm-policies" |
+#jq -r '.data[] | "\(.name)\t\(.id)\t\(.active)"'
